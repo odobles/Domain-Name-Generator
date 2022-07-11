ae4087922e7550b274335113ef18b8fb5513ee96
odobles <Johel_Dobles.16@hotmail.com>
2022-07-11 03:41:57 +0000

    Update2

--- src/app.js
+++ src/app.js
@@ -1,17 +1,22 @@
-let sujeto = ['The dog', 'My grandma', 'His turtle', 'My bird'];
-let accion = ['ate', 'peed', 'crushed', 'broke', 'stole'];
-let objeto = ['my homework', 'my keys', 'the car', 'my PC'];
-let tiempo = ['before the class', 'before leaving', 'when I finished', 'during my lunch'];
+let sujeto = ["The dog", "My grandma", "His turtle", "My bird"];
+let accion = ["ate", "peed", "crushed", "broke", "stole"];
+let objeto = ["my homework", "my keys", "the car", "my PC"];
+let tiempo = [
+  "before the class",
+  "before leaving",
+  "when I finished",
+  "during my lunch"
+];
 
-function randomNum(item)
-{
-  num = Math.floor(Math.random()*item.length);
+var builtExcuse = [];
+let listadelistas = [sujeto, accion, objeto, tiempo];
+
+function randomNum(item) {
+  let num = Math.floor(Math.random() * item.length);
   builtExcuse.push(item[num]);
 }
 
-let listadelistas = [sujeto, accion, objeto, tiempo];
-
-builtExcuse = [];
+// builtExcuse = [];
 listadelistas.forEach(randomNum);
 
 let excusa = builtExcuse.join(" ");

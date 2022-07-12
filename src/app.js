a97ebe14ace0398c1b585ca8194351bbdc245c83
odobles <108157704+odobles@users.noreply.github.com>
2022-07-11 19:42:34 -0600

    first commit

--- src/app.js
+++ src/app.js
@@ -1,70 +1,20 @@
-let sujeto = [
-  "The dog",
-  "My grandma",
-  "Elon Musk",
-  "Rasputin",
-  "The Peaky Blinders",
-  "Aliens",
-  "A cow",
-  "A dinosaur",
-  "An old man",
-  "John Wick",
-  "Shrek",
-  "Obama",
-  "Bin Laden",
-  "Zoboomafoo",
-  "Chewbacca",
-  "Nicolas Maduro"
-];
-let accion = [
-  "ate",
-  "peed",
-  "destroyed",
-  "stole",
-  "sold",
-  "burned",
-  "lost",
-  "buried"
-];
-let objeto = [
-  "my homework",
-  "my keys",
-  "my car",
-  "my PC",
-  "my house",
-  "my room",
-  "my notes",
-  "my ID",
-  "my driving license",
-  "my fridge",
-  "my grandpa",
-  "my money",
-  "my toilet"
-];
-let tiempo = [
-  "before the class",
-  "before leaving this morning",
-  "this morning",
-  "during my lunch",
-  "while I was away",
-  "the other day",
-  "when I was asleep",
-  "during break",
-  "during vacations"
-];
+let pronoun = ["the", "our"];
+let adj = ["great", "big", "only", "amazing", "lordOf", "masterOf"];
+let noun = ["jogger", "raccoon", "anime", "coffee", "cats" ];
+let tld = [".net", ".com",".gov",".me", ".dev", ".us"]
 
-var builtExcuse = [];
-let listadelistas = [sujeto, accion, objeto, tiempo];
+var construirDominio = [];
+let listadelistas = [pronoun, adj, noun, tld];
 
 function randomNum(item) {
   let num = Math.floor(Math.random() * item.length);
-  builtExcuse.push(item[num]);
+  construirDominio.push(item[num]);
 }
 
 listadelistas.forEach(randomNum);
 
-let excusa = builtExcuse.join(" ");
-let excusaDOM = document.getElementById("excusa");
-excusaDOM.innerText = excusa;
+let domainName = construirDominio.join(" ");
+let domainDOM = document.getElementById("domainName");
+domainDOM.innerText = domainName;
 
 // console.log(builtExcuse.join(" "))
